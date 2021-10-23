eee89630742ee3e663f5bb4e54c030e9497673aa
phLearnGit_Glb <PHZS_glb@stu.tyust.edu.cn>
2021-10-23 11:01:53 +0800

    The first operation git of IDEA

---
+++ src/main/java/com/phlearn_git/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.phlearn_git.git;
+
+/**
+ * @author PH~
+ * @version 1.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("第一个使用IDEA操作Git");
+    }
+}
