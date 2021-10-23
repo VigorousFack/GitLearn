28f457316bde3fbd005ee8c3ad7090edc8905c99
phLearnGit_Glb <PHZS_glb@stu.tyust.edu.cn>
2021-10-23 11:03:33 +0800

    测试切换版本2

--- src/main/java/com/phlearn_git/git/GitTest.java
+++ src/main/java/com/phlearn_git/git/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("第一个使用IDEA操作Git");
         System.out.println("帅浩第二次使用IDEA操作Git");
+        System.out.println("帅浩第三次使用IDEA操作Git");
     }
 }
